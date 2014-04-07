04d1f12a27b2efec9e1e86989aab27f979ee669d
Christian Weyer <christian.weyer@thinktecture.com>
2014-04-07 16:03:41 +0200

    Bit of cleanup

--- Client/Services/SessionService/SessionServiceClient.custom.cs
+++ Client/Services/SessionService/SessionServiceClient.custom.cs
@@ -2,6 +2,7 @@
 using System.Collections.ObjectModel;
 using System.Configuration;
 using System.Diagnostics;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR.Client;
